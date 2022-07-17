0c71233f957bd40fabe8fe36b49f9b73a6adb53c
rkitonlinetrainings <rkitonlinetrainings@gmail.com>
2022-07-17 21:45:46 +0530

    initial commit

---
+++ src/components/AnimatedText.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+const homeData = {
+  skills: ["Designer", "Developer", "Freelancer"],
+};
+const AnimatedText = () => {
+  const [text, setText] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setText(text < 2 ? text + 1 : 1);
+    }, 5000);
+    return () => clearInterval(interval);
+  });
+  return (
+    <span className="cd-headline rotate-1">
+      <span className="blc">Creative </span>
+      <span className="cd-words-wrapper">
+        {homeData.skills.map((skill, i) => (
+          <b key={i} className={text === i ? "is-visible" : "is-hidden"}>
+            {skill}
+          </b>
+        ))}
+      </span>
+    </span>
+  );
+};
+export default AnimatedText;
---
+++ src/components/Contact.js
@@ -0,0 +1,111 @@
+const Contact = () => {
+  return (
+    <div className="edrea_tm_section hidden animated" id="contact">
+      <div className="section_inner">
+        <div className="edrea_tm_contact">
+          <div className="edrea_tm_main_title">
+            <h3>
+              Get in <span className="coloring">Touch</span>
+            </h3>
+          </div>
+          <div className="wrapper">
+            <div className="left">
+              <ul>
+                <li>
+                  <div className="list_inner">
+                    <i className="icon-location" />
+                    <span>
+                      <a href="#" className="href_location">
+                        Avon str. 22, NYC, USA
+                      </a>
+                    </span>
+                  </div>
+                </li>
+                <li>
+                  <div className="list_inner">
+                    <i className="icon-phone" />
+                    <span>
+                      <a href="#">+77 022 155 02 02</a>
+                    </span>
+                  </div>
+                </li>
+                <li>
+                  <div className="list_inner">
+                    <i className="icon-mail-1" />
+                    <span>
+                      <a href="mailto:example@gmail.com">example@gmail.com</a>
+                    </span>
+                  </div>
+                </li>
+                <li>
+                  <div className="list_inner">
+                    <i className="icon-facebook-rect" />
+                    <span>
+                      <a href="https://www.facebook.com">@facebookNick</a>
+                    </span>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div className="right">
+              <div className="fields">
+                <form
+                  action="/"
+                  method="post"
+                  className="contact_form"
+                  id="contact_form"
+                >
+                  <div
+                    className="returnmessage"
+                    data-success="Your message has been received, We will contact you soon."
+                  />
+                  <div className="empty_notice">
+                    <span>Please Fill Required Fields</span>
+                  </div>
+                  <div className="first">
+                    <ul>
+                      <li>
+                        <div className="list_inner">
+                          <input
+                            id="name"
+                            type="text"
+                            placeholder="Name"
+                            autoComplete="off"
+                          />
+                        </div>
+                      </li>
+                      <li>
+                        <div className="list_inner">
+                          <input
+                            id="email"
+                            type="text"
+                            placeholder="Email"
+                            autoComplete="off"
+                          />
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="last">
+                    <textarea
+                      id="message"
+                      placeholder="Message"
+                      defaultValue={""}
+                    />
+                  </div>
+                  <div className="edrea_tm_button">
+                    <a id="send_message" href="#">
+                      Send Message
+                    </a>
+                  </div>
+                  {/* If you want change mail address to yours, just open "modal" folder >> contact.php and go to line 4 and change detail to yours.  */}
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Contact;
